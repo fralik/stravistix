4aa70e3da2b41a38567c1fb705ea5cc8d3e22194
Thomas Champagne <th.champagne@gmail.com>
2015-05-01 14:52:32 +0200

    Update env.js
    Hey KamensekD,
    
    Could you use your own google analytics inside your fork?
    
    Thanks for all the work you put in this fork !
    
    Cheers,
    
    Tom

--- hook/extension/config/env.js
+++ hook/extension/config/env.js
@@ -1,7 +1,7 @@
 var env = {
     preview: false, // Must be false in release
     buildNumber: 601, // 0.6.0.1
-    analyticsTrackingID: 'UA-51167057-4', // prod:'UA-51167057-4', test:'UA-51167057-5'
+    analyticsTrackingID: 'change_with_yours', // prod:'UA-51167057-4', test:'UA-51167057-5'
     forceUpdated: false, // Must be false in release
     debugMode: false, // Must be false in release
     useActivityStreamCache: true // Must be true in release
